18ecb763e8322eabd9021cd0d7d12747ea7eee55
Raymond Tam <raymond.tam@us.hctx.net>
2021-06-21 13:45:19 -0500

    Step 3 B

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,25 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
 {
-    public class CelestialObjectController : Controller
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
-        }
-
-        [Route("")]
-        public IActionResult Index()
-        {
-            return View();
         }
     }
 }
