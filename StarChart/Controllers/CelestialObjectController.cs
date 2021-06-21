742957d078b84445afb76c8da132c1acda91f9b4
Raymond Tam <raymond.tam@us.hctx.net>
2021-06-21 14:12:18 -0500

    Step 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using System.Linq;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +13,16 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Where(w => w.Id == id);
+            if (celestialObject == null) return NotFound();
+
+            
+            return Ok();
         }
     }
 }
